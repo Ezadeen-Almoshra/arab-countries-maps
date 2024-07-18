9da651b5d6610b9147162f3c3e84b28cc57e807b
Ezadeen-Almoshra <ezadeenuniversity288@gmail.com>
2024-07-18 17:48:53 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/arab-countries-maps/sw.js', { scope: '/arab-countries-maps/' })})}
